3dbea11500ed9b66ec0c7ef6422703c6dad5c685
Khan / 이창민 <EeeasyCode@gmail.com>
2024-10-14 17:42:03 +0900

    chore: api-gateway에서 동아리 서비스 swagger 연동

--- club-service/src/main/java/club/gach_dong/config/SwaggerConfig.java
+++ club-service/src/main/java/club/gach_dong/config/SwaggerConfig.java
@@ -3,6 +3,7 @@ package club.gach_dong.config;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -13,7 +14,8 @@ public class SwaggerConfig {
     public OpenAPI openAPI() {
         return new OpenAPI()
                 .components(new Components())
-                .info(apiInfo());
+                .info(apiInfo())
+                .addServersItem(serverItem());
     }
 
     private Info apiInfo() {
@@ -21,5 +23,11 @@ public class SwaggerConfig {
                 .title("가츠동 API 명세 - 동아리 서비스")
                 .description("동아리 서비스에 대한 API 명세입니다.")
                 .version("v1");
+    }
+
+    private Server serverItem() {
+        return new Server()
+                .url("http://localhost:8081/club/")
+                .description("동아리 서비스 URL");
     }
 }
