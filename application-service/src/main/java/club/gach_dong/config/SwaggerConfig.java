28873b345c956002355384feb2437e5fe9be3fd0
opp-13 <57740411+opp-13@users.noreply.github.com>
2024-10-21 14:38:41 +0900

    Fix: 임시 swagger 설정 삭제

--- application-service/src/main/java/club/gach_dong/config/SwaggerConfig.java
+++ application-service/src/main/java/club/gach_dong/config/SwaggerConfig.java
@@ -10,52 +10,28 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
-//public class SwaggerConfig {
-
-//    @Value("${app.gateway.endpoint}") String gatewayEndpoint;
-//    @Bean
-//    public OpenAPI openAPI() {
-//        return new OpenAPI()
-//                .components(new Components())
-//                .info(apiInfo())
-//                .addServersItem(serverItem());
-//    }
-//
-//    private Info apiInfo() {
-//        return new Info()
-//                .title("가츠동 API 명세 - 지원 서비스")
-//                .description("지원 서비스에 대한 API 명세입니다.")
-//                .version("v1");
-//    }
-//
-//    private Server serverItem() {
-//        return new Server()
-//                .url(gatewayEndpoint + "/application/")
-//                .description("지원 서비스 URL");
-//    }
-//}
-
 @Configuration
 public class SwaggerConfig {
 
+    @Value("${app.gateway.endpoint}") String gatewayEndpoint;
     @Bean
     public OpenAPI openAPI() {
-        Info info = new Info()
+        return new OpenAPI()
+                .components(new Components())
+                .info(apiInfo())
+                .addServersItem(serverItem());
+    }
+
+    private Info apiInfo() {
+        return new Info()
                 .title("가츠동 API 명세 - 지원 서비스")
                 .description("지원 서비스에 대한 API 명세입니다.")
-                .version("1.0.0");
-        String jwtSchemeName = "JWT TOKEN";
-        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwtSchemeName);
-        Components components = new Components()
-                .addSecuritySchemes(jwtSchemeName, new SecurityScheme()
-                        .name(jwtSchemeName)
-                        .type(SecurityScheme.Type.HTTP)
-                        .scheme("bearer")
-                        .bearerFormat("JWT"));
-        return new OpenAPI()
-                .info(info)
-                .addSecurityItem(securityRequirement)
-                .components(components);
+                .version("v1");
+    }
+
+    private Server serverItem() {
+        return new Server()
+                .url(gatewayEndpoint + "/application/")
+                .description("지원 서비스 URL");
     }
 }
