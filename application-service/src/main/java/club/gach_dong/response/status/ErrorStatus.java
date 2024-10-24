96bbb1781fde4870a3fdd369389eea93a656b33a
opp-13 <57740411+opp-13@users.noreply.github.com>
2024-10-24 17:41:03 +0900

    Style:  Error Status 코드(APPLICATION401) 중복 문제 해결

--- application-service/src/main/java/club/gach_dong/response/status/ErrorStatus.java
+++ application-service/src/main/java/club/gach_dong/response/status/ErrorStatus.java
@@ -14,7 +14,7 @@ public enum ErrorStatus {
     _FORBIDDEN(HttpStatus.FORBIDDEN, "COMMON403", "금지된 요청입니다."),
     _EMPTY_FIELD(HttpStatus.NO_CONTENT, "COMMON404", "입력 값이 누락되었습니다."),
     
-    CLUB_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "APPLICATION401", "인증이 필요합니다."),
+    CLUB_UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "CLUBADMIN401", "인증이 필요합니다."),
 
     APPLICATION_FORM_NOT_FOUND(HttpStatus.NOT_FOUND, "APPLICATIONFORM401", "ApplicationForm이 없습니다."),
     APPLICATION_FORM_IN_USE(HttpStatus.CONFLICT, "APPLICATIONFORM402", "이미 사용중인 지원서 양식입니다."),
