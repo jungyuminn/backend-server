4c5eccdd92352f2ece607f483121cdaffaa125a1
Khan / 이창민 <EeeasyCode@gmail.com>
2024-10-24 16:21:14 +0900

    chore: ClubResponse의 필드 값 수정

--- club-service/src/main/java/club/gach_dong/dto/response/ClubResponse.java
+++ club-service/src/main/java/club/gach_dong/dto/response/ClubResponse.java
@@ -23,7 +23,7 @@ public record ClubResponse(
         boolean recruitingStatus,
 
         @Schema(description = "동아리 설명", example = "가츠동은 다양한 활동을 하는 동아리입니다.")
-        String description,
+        String introduction,
 
         @Schema(description = "설립일", example = "2023-01-01T00:00:00")
         LocalDateTime establishedAt,
@@ -31,6 +31,7 @@ public record ClubResponse(
         @Schema(description = "업데이트일", example = "2023-01-01T00:00:00")
         LocalDateTime updatedAt
 ) {
+
     public static ClubResponse from(Club club) {
         return new ClubResponse(
                 club.getName(),
@@ -38,9 +39,10 @@ public record ClubResponse(
                 club.getShortDescription(),
                 club.getClubImageUrl(),
                 club.isRecruitingStatus(),
-                club.getDescription(),
+                club.getIntroduction(),
                 club.getEstablishedAt(),
                 club.getUpdatedAt()
         );
     }
 }
+
