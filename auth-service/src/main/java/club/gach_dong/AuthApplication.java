184785bd5d86abe6d8db24192b9ac2d032a6b2a0
Jung <jungym0419@gmail.com>
2024-10-29 18:50:35 +0900

    fix: Application name 수정

--- auth-service/src/main/java/club/gach_dong/AuthApplication.java
+++ auth-service/src/main/java/club/gach_dong/AuthApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class GachDongApplication {
+public class AuthApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GachDongApplication.class, args);
