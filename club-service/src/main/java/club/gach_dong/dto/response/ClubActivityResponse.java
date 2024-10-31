3324999d454d048563f8cc348360c6fbfbaf5d94
Khan / 이창민 <EeeasyCode@gmail.com>
2024-10-31 15:24:12 +0900

    refactor: Response field 에 @NotNull 어노테이션 추가

--- club-service/src/main/java/club/gach_dong/dto/response/ClubActivityResponse.java
+++ club-service/src/main/java/club/gach_dong/dto/response/ClubActivityResponse.java
@@ -3,16 +3,20 @@ package club.gach_dong.dto.response;
 import club.gach_dong.domain.Activity;
 import io.swagger.v3.oas.annotations.media.Schema;
 
+import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
 
 public record ClubActivityResponse (
         @Schema(description = "활동 제목", example = "2024년 봄 캠프")
+        @NotNull
         String title,
 
         @Schema(description = "활동 날짜", example = "2024-04-12")
+        @NotNull
         LocalDate date,
 
         @Schema(description = "활동 설명", example = "봄 캠프에서 다양한 활동을 했습니다.")
+        @NotNull
         String description
 ) {
     public static ClubActivityResponse from(Activity activity) {
--- club-service/src/main/java/club/gach_dong/dto/response/ClubContactInfoResponse.java
+++ club-service/src/main/java/club/gach_dong/dto/response/ClubContactInfoResponse.java
@@ -2,12 +2,15 @@ package club.gach_dong.dto.response;
 
 import club.gach_dong.domain.ContactInfo;
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotNull;
 
 public record ClubContactInfoResponse(
         @Schema(description = "연락 수단 (예: gmail, phone)", example = "gmail")
+        @NotNull
         String contactMethod,
 
         @Schema(description = "연락처 정보", example = "gachdong@gmail.com")
+        @NotNull
         String contactValue
 ) {
     public static ClubContactInfoResponse from(ContactInfo contactInfo) {
