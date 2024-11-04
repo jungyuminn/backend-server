dc9199f40ff10f0502198a80f3d5724f4c8b00a5
Khan / 이창민 <EeeasyCode@gmail.com>
2024-11-04 16:55:01 +0900

    fix: club-category 수정

--- club-service/src/main/java/club/gach_dong/domain/ClubCategory.java
+++ club-service/src/main/java/club/gach_dong/domain/ClubCategory.java
@@ -1,5 +1,16 @@
 package club.gach_dong.domain;
 
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
 public enum ClubCategory {
-    ART, SPORTS, SCIENCE, MUSIC, TECH, OTHER
+    ACADEMIC("학술 · 사회"),
+    EXHIBITION("전시 · 취미"),
+    SPORTS("체육"),
+    PERFORMANCE("공연"),
+    MUSIC("음악"),
+    RELIGION("종교"),
+    OTHER("기타");
+
+    private final String text;
 }
