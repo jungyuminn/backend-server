baaa0f8c62f0231434439ca5d8e6380837c2114f
Khan / 이창민 <EeeasyCode@gmail.com>
2024-11-05 20:45:01 +0900

    chore: swagger의 ArrayResponse results 가 nullable로 처리되지 않도록 설정

--- club-service/src/main/java/club/gach_dong/dto/response/ArrayResponse.java
+++ club-service/src/main/java/club/gach_dong/dto/response/ArrayResponse.java
@@ -1,8 +1,10 @@
 package club.gach_dong.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.Collection;
 
 public record ArrayResponse<T>(
+        @Schema(description = "결과 목록", nullable = false)
         Collection<T> results
 ) {
     public static <T> ArrayResponse<T> of(Collection<T> results) {
