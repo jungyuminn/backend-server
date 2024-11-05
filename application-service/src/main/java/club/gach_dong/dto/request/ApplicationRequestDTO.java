99967089236abcb04f435c02d161039166f6944f
opp-13 <57740411+opp-13@users.noreply.github.com>
2024-11-05 16:44:53 +0900

    comment: Swagger에 DTO에 대한 설명 추가

--- application-service/src/main/java/club/gach_dong/dto/request/ApplicationRequestDTO.java
+++ application-service/src/main/java/club/gach_dong/dto/request/ApplicationRequestDTO.java
@@ -1,6 +1,7 @@
 package club.gach_dong.dto.request;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
@@ -9,48 +10,85 @@ import lombok.Getter;
 
 public class ApplicationRequestDTO {
 
+    @Schema(description = "지원서 양식 생성 요청 DTO")
     @Getter
     public static class ToCreateApplicationFormDTO {
+        @Schema(description = "지원 ID", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false, example = "3")
         @NotNull(message = "ApplyId가 누락되었습니다.")
         private Long applyId;
 
+        @Schema(description = "지원서 양식 상태(임시저장/저장)", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false, example = "SAVED")
         @NotNull(message = "지원 양식 상태가 누락되었습니다.")
         @Pattern(regexp = "TEMPORARY_SAVED|SAVED", message = "지원 양식 상태는 TEMPORARY_SAVED 또는 SAVED 이어야 합니다.")
         private String status;
 
+        @Schema(description = "지원서 양식 ID", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false, example = "가츠동 지원서 양식")
         @NotNull(message = "지원서 이름이 누락되었습니다.")
         @Size(max = 50, message = "지원서 이름은 50자 이내여야 합니다.")
         private String formName;
 
+        @Schema(
+                description = "지원서 양식 본문, Json 형식으로 넣어주세요.",
+                requiredMode = Schema.RequiredMode.REQUIRED,
+                nullable = false,
+                example = "{\n" +
+                        "  \"name\": \"이름을 넣어주세요\",\n" +
+                        "  \"age\": \"나이를 넣어주세요\",\n" +
+                        "  \"education\": {\n" +
+                        "    \"university\": \"출신 학교를 넣어주세요\",\n" +
+                        "    \"major\": \"전공을 넣어주세요\"\n" +
+                        "  }\n" +
+                        "}"
+        )
         @NotNull(message = "지원서 본문이 누락되었습니다.")
         @Size(max = 2000, message = "지원서 본문이 너무 큽니다.")
         @JsonProperty("formBody")
         private Map<String, Object> formBody;
     }
 
+    @Schema(description = "동아리 지원 요청 DTO")
     @Getter
     public static class ToApplyClubDTO {
+        @Schema(description = "지원 ID", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false, example = "3")
         @NotNull(message = "지원서 Id가 누락되었습니다.")
         private Long applicationFormId;
 
+        @Schema(
+                description = "지원서 답변 본문, Json 형식으로 넣어주세요.",
+                requiredMode = Schema.RequiredMode.REQUIRED,
+                nullable = false,
+                example = "{\n" +
+                        "  \"name\": \"가츠동\",\n" +
+                        "  \"age\": 25,\n" +
+                        "  \"education\": {\n" +
+                        "    \"university\": \"가천대학교\",\n" +
+                        "    \"major\": \"컴퓨터공학\"\n" +
+                        "  }\n" +
+                        "}"
+        )
         @Size(max = 30000, message = "지원서 답변이 너무 큽니다.")
         @JsonProperty("formBody")
         private Map<String, Object> formBody;
 
+        @Schema(description = "지원서 상태(임시저장/저장/수정 가능)", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false, example = "SAVED")
         @NotNull(message = "지원 상태가 누락되었습니다.")
         @Pattern(regexp = "TEMPORARY_SAVED|SAVED|SAVED_CHANGEABLE", message = "지원 상태는 TEMPORARY_SAVED 또는 SAVED, SAVED_CHANGEABLE 이어야 합니다.")
         private String status;
 
+        @Schema(description = "지원할 동아리 이름", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false, example = "가츠동")
         @NotNull(message = "동아리 이름이 누락되었습니다.")
         @Size(max = 80, message = "동이리 이름이 너무 큽니다.")
         private String clubName;
     }
 
+    @Schema(description = "동아리 지원자 상태 수정 요청 DTO")
     @Getter
     public static class ToChangeApplicationStatus {
+        @Schema(description = "변경할 지원 ID", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false, example = "2")
         @NotNull(message = "변경할 Application Id가 누락되었습니다.")
         private Long applicationId;
 
+        @Schema(description = "변경할 지원 상태", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false, example = "서류 합격")
         @NotNull(message = "변경할 application 상태를 입력해 주세요.")
         @Size(max = 80, message = "상태가 너무 깁니다.")
         private String status;
--- application-service/src/main/java/club/gach_dong/dto/response/ApplicationResponseDTO.java
+++ application-service/src/main/java/club/gach_dong/dto/response/ApplicationResponseDTO.java
@@ -1,5 +1,6 @@
 package club.gach_dong.dto.response;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import java.util.List;
 import java.util.Map;
 import lombok.Builder;
@@ -8,44 +9,69 @@ import lombok.Getter;
 public class ApplicationResponseDTO {
     @Getter
     @Builder
+    @Schema(description = "관리자용 양식 조회 결과 반환 DTO")
     public static class ToGetFormInfoAdminDTO {
+        @Schema(description = "지원서 양식 ID", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false)
         private Long formId;
+
+        @Schema(description = "지원서 양식 이름", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false)
         private String formName;
+
+        @Schema(description = "지원서 양식 내용(Json)", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false)
         private Map<String, Object> formBody;
+
+        @Schema(description = "지원서 양식 상태 (임시 저장인지, 삭제 가능한지 등)", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false)
         private String formStatus;
     }
 
     @Getter
     @Builder
+    @Schema(description = "사용자용 양식 조회 결과 반환 DTO")
     public static class ToGetFormInfoUserDTO {
+        @Schema(description = "지원서 양식 ID", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false)
         private Long formId;
+
+        @Schema(description = "지원서 양식 이름", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false)
         private String formName;
+
+        @Schema(description = "지원서 양식 내용(Json)", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false)
         private Map<String, Object> formBody;
     }
 
     @Getter
     @Builder
+    @Schema(description = "지원 내역 목록 조회 결과 반환 DTO")
     public static class ToGetApplicationHistoryListDTO {
         private List<ToGetApplicationHistoryDTO> toGetApplicationHistoryDTO;
     }
 
     @Getter
     @Builder
+    @Schema(description = "지원 내역 상세 조회 결과 반환 DTO")
     public static class ToGetApplicationHistoryDTO {
+        @Schema(description = "지원 ID", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false)
         private Long applicationId;
+
+        @Schema(description = "지원한 동아리 이름", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false)
         private String clubName;
+
+        @Schema(description = "지원 상태(합격, 불합격, 서류 합격 등)", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false)
         private String status;
     }
 
     @Getter
     @Builder
+    @Schema(description = "지원서 양식 생성 결과 반환 DTO")
     public static class ToCreateApplicationFormDTO {
+        @Schema(description = "생성된 지원서 양식 ID", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false)
         private Long applicationFormId;
     }
 
     @Getter
     @Builder
+    @Schema(description = "동아리 지원 결과 반환 DTO")
     public static class ToCreateApplicationDTO {
+        @Schema(description = "접수된 지원 ID", requiredMode = Schema.RequiredMode.REQUIRED, nullable = false)
         private Long applyId;
     }
 
