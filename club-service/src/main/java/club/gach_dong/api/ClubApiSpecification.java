9ecc8f76e0b3afc3f43edd39c829e4c6bdf0c95c
Khan / 이창민 <EeeasyCode@gmail.com>
2024-11-06 16:30:48 +0900

    chore: admin, 인증/인가, pulbic api 분리

--- club-service/src/main/java/club/gach_dong/api/ClubApiSpecification.java
+++ club-service/src/main/java/club/gach_dong/api/ClubApiSpecification.java
@@ -1,15 +1,6 @@
 package club.gach_dong.api;
 
-
-import club.gach_dong.dto.request.CreateClubRequest;
-import club.gach_dong.dto.response.*;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
-import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
@@ -17,16 +8,4 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/v1")
 @Validated
-public interface ClubApiSpecification {
-    @Operation(
-            summary = "동아리 생성",
-            description = "동아리 정보를 입력받아 동아리를 생성합니다.",
-            security = @SecurityRequirement(name = "Authorization")
-    )
-    @PostMapping("/create")
-    ResponseEntity<ClubResponse> createClub(
-            @Valid
-            @RequestBody
-            CreateClubRequest createClubRequest
-    );
-}
+public interface ClubApiSpecification {}
--- club-service/src/main/java/club/gach_dong/controller/ClubController.java
+++ club-service/src/main/java/club/gach_dong/controller/ClubController.java
@@ -1,23 +1,9 @@
 package club.gach_dong.controller;
 
 import club.gach_dong.api.ClubApiSpecification;
-import club.gach_dong.dto.request.CreateClubRequest;
-import club.gach_dong.dto.response.*;
-import club.gach_dong.service.ClubService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 @Controller
 @RequiredArgsConstructor
-public class ClubController implements ClubApiSpecification {
-
-    private final ClubService clubService;
-
-    @Override
-    public ResponseEntity<ClubResponse> createClub(CreateClubRequest createClubRequest) {
-        ClubResponse clubResponse = clubService.createClub(createClubRequest);
-        return new ResponseEntity<>(clubResponse, HttpStatus.CREATED);
-    }
-}
+public class ClubController implements ClubApiSpecification {}
