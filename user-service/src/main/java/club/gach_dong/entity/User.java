4df732540db98d56f9f9cd1f52d21a2c85f524ba
Jung <jungym0419@gmail.com>
2024-11-07 02:29:01 +0900

    chore: table명 수정

--- user-service/src/main/java/club/gach_dong/entity/User.java
+++ user-service/src/main/java/club/gach_dong/entity/User.java
@@ -7,7 +7,7 @@ import lombok.NoArgsConstructor;
 @Entity
 @Data
 @NoArgsConstructor
-@Table(name = "user_profileImage")
+@Table(name = "user")
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
