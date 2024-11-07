a37fe7adb9c495a25bee7ed6a07b11ab8faedc61
jungyuminn <jungym0419@gmail.com>
2024-11-07 12:57:00 +0900

    fix: gcpStorageConfig 오타 수정

--- user-service/src/main/java/club/gach_dong/config/GcpStorageConfig.java
+++ user-service/src/main/java/club/gach_dong/config/GcpStorageConfig.java
@@ -29,15 +29,14 @@ public class GcpStorageConfig {
         );
 
         return StorageOptions.newBuilder()
-                .setCredentials(credentialsPath)
+                .setCredentials(credentials)
                 .setProjectId(projectId)
                 .build()
                 .getService();
     }
-}
 
-@Bean
-public String bucketName() {
-    return bucketName;
-}
+    @Bean
+    public String bucketName() {
+        return bucketName;
+    }
 }
