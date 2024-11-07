b606f4d8a2a25c874343832f3f9afe131dbebae5
jungyuminn <jungym0419@gmail.com>
2024-11-07 13:11:37 +0900

    fix: import문 수정

--- user-service/src/main/java/club/gach_dong/config/GcpStorageConfig.java
+++ user-service/src/main/java/club/gach_dong/config/GcpStorageConfig.java
@@ -7,8 +7,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class GcpStorageConfig {
