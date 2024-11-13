4559b93e6e3ec680b761196c6ad71e1f72daa549
Khan / 이창민 <EeeasyCode@gmail.com>
2024-11-14 08:46:10 +0900

    fix: queryParam -> PathVariable 수정

--- club-service/src/main/java/club/gach_dong/api/ClubAdminApiSpecification.java
+++ club-service/src/main/java/club/gach_dong/api/ClubAdminApiSpecification.java
@@ -16,14 +16,13 @@ import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import jakarta.websocket.server.PathParam;
 import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @Tag(name = "Admin 동아리 API", description = "동아리 관리자 관련 API")
@@ -107,7 +106,7 @@ public interface ClubAdminApiSpecification {
     Boolean hasAuthority(
             @RequestUserReferenceId
             String userReferenceId,
-            @PathParam("clubId")
+            @PathVariable("clubId")
             Long clubId
     );
 
@@ -120,7 +119,7 @@ public interface ClubAdminApiSpecification {
     Boolean hasAuthorityByRecruitmentId(
             @RequestUserReferenceId
             String userReferenceId,
-            @PathParam("recruitmentId")
+            @PathVariable("recruitmentId")
             Long recruitmentId
     );
 
@@ -131,7 +130,7 @@ public interface ClubAdminApiSpecification {
     )
     @GetMapping("/recruitment/{recruitmentId}/is-valid")
     Boolean isValidRecruitment(
-            @RequestParam("recruitmentId")
+            @PathVariable("recruitmentId")
             Long recruitmentId
     );
 
